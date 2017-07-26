181bd7441003f4b1677fb26029d451c5e3fb4b2a
Alex <ignatenko2207@gmail.com>
2017-07-26 20:23:36 +0300

    Add user class

---
+++ SpringHiberStarter/src/main/java/org/itstep/dao/pojo/User.java
@@ -0,0 +1,44 @@
+package org.itstep.dao.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID", unique = true, nullable = false)
+	private Long userId;
+	
+	@Column(name = "EMAIL", unique = true, nullable = false)
+	private String email;
+	
+	@Column(name = "PASSWORD", nullable = false)
+	private String password;
+	
+	@Column(name = "FIRST_NAME", nullable = false)
+	private String firstName;
+	
+	@Column(name = "SECOND_NAME")
+	private String secondName;
+	
+	@Column(name = "PHONE", unique = true)
+	private String phone;
+	
+	@Column(name = "ADDRESS")
+	private String address;
+	
+	@Column(name = "LAST_ACTION", nullable = false)
+	private Long lastAction;
+	
+
+}
--- SpringHiberStarter/src/main/java/org/itstep/util/HibernateUtil.java
+++ SpringHiberStarter/src/main/java/org/itstep/util/HibernateUtil.java
@@ -4,14 +4,18 @@ import java.io.File;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 	 
     private static SessionFactory buildSessionFactory() {
-        try {
-            return new AnnotationConfiguration().configure(new File("C:\\workspace\\hibernate-test-project\\hibernate.cgf.xml")).buildSessionFactory();
- 
+    	String sep = System.getProperty("file.separator");
+    	try {
+            return new AnnotationConfiguration().configure(
+            		new File("src"+sep+"main"+sep+"java"+sep+
+            				"org"+sep+"itstep"+sep+"config"+sep+"hibernate.cgf.xml")).buildSessionFactory();
         }
         catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
